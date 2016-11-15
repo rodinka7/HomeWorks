1ac19ff0b553763bb72db915d1eb13a65d5921fe
rodinka7 <rodinka7@gmail.com>
2016-11-15 16:41:32 +0200

    Reduce improvement

--- hm3/01/reduce.js
+++ hm3/01/reduce.js
@@ -5,21 +5,26 @@ let array = [1,2,3,4,5,6],
 	res;
 
 let reduce = (arr, item, callback) => {	
-	var i = item;
+	var i;
+
+	if (item === 0) {
+		i = 1
+	} else i = item
 
 	while (i < arr.length) {
 
-		res = callback(arr[i]);
+		console.log(arr[i-1], arr[i])
+		res = callback(arr[i-1], arr[i]);
 
-		i++
+		i += 2;
 	}
 
 	return res  
 }
 
-console.log(reduce(array, 0, function(curNum){
+console.log(reduce(array, 0, function(prevNum, curNum){
 	
-	prevSum +=curNum;
+	prevSum +=  prevNum + curNum;
 
 	return prevSum; 
 }));
