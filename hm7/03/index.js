8e0b50b3b4561bda6ff846b4c8142717ec9fda65
rodinka7 <rodinka7@gmail.com>
2016-11-29 13:50:49 +0200

    The third part

--- hm7/03/index.js
+++ hm7/03/index.js
@@ -33,19 +33,19 @@
 			randomLeft = (width) => {
 				return Math.round(Math.random()*(wrapper.clientWidth - width));
 			}
-
-			obj[`figure${k}`] = {};
 			
 			div.style.position = 'absolute';
-			obj[`figure${k}`].height = div.style.height = randomHeight() + 'px';
-			obj[`figure${k}`].width = div.style.width = randomWidth() + 'px';
+			div.style.height = randomHeight() + 'px';
+			div.style.width = randomWidth() + 'px';
 			div.style.top = randomTop(randomHeight()) + 'px';
 			div.style.left = randomLeft(randomWidth())  + 'px';
-			obj[`figure${k}`].color = div.style.background = randomColor();
+			div.style.background = randomColor();
 			div.style.cursor = 'pointer'; 
 
 			this.appendChild(div);
 
+			console.log(div.style.top)
+				console.log(div.style.left)
 			div.addEventListener('mousedown', function(e) {
 				this.onmousemove = function(e){
 					moveIt(e);
@@ -59,18 +59,36 @@
 					div.style.left = x + 'px';
 					div.style.transform = 'translate(-50%,-50%)';
 				};
-
 				this.onmouseup = function(e){
 					this.onmousemove = null;
 				}
 			});
-
-			obj[`figure${k}`].top = div.style.top;
-			obj[`figure${k}`].left = div.style.left;
-			k++;
 		};
 
 		if(e.target.classList.contains('save')){
+			let divs = wrapper.querySelectorAll('div'),
+				arr = [];
+
+			[].map.call(divs, function(item){
+				if(!item.classList.contains('container')){
+					return arr.push(item);
+				}
+			});
+
+			arr.forEach(function(item){
+				obj[`figure${k}`] = {};
+				obj[`figure${k}`]['height'] = item.style.height;
+				obj[`figure${k}`]['width'] = item.style.width;
+				obj[`figure${k}`]['top'] = item.style.top;
+				obj[`figure${k}`]['left'] = item.style.left;
+				obj[`figure${k}`]['color'] = item.style.background;
+				k++;
+				console.log(item.style.top)
+				console.log(item.style.left)
+				console.log(item.offsetTop)
+				console.log(item.offsetLeft)
+			})
+
 			document.cookie = `cookie=${JSON.stringify(obj)}`;
 			alert('Изменения сохранены!')
 		}
@@ -83,13 +101,16 @@
 			let div = document.createElement('div');
 
 			div.style.position = 'absolute';
-			div.style.background = obj[item].color;
-			div.style.height = obj[item].height;
-			div.style.left = obj[item].left;
-			div.style.top = obj[item].top;
-			div.style.width = obj[item].width;
+			div.style.background = obj[item]['color'];
+			div.style.height = obj[item]['height'];
+			div.style.left = obj[item]['left'];
+			div.style.top = obj[item]['top'];
+			div.style.width = obj[item]['width'];
 			
 			wrapper.appendChild(div);
+
+			console.log(div.offsetTop)
+			console.log(div.offsetLeft)
 		}
 	})
 })();
